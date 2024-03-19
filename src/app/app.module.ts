a098c0343797e6e1a781575d48d56d482eb72540
PereAnd <aa.pereza1@uniandes.edu.co>
2024-03-19 16:14:45 -0500

    Pantalla Completar información

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BackgroundComponent } from './components/background/background.component';
+import { ChecklistinfoComponent } from './components/checklistinfo/checklistinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BackgroundComponent,
+    ChecklistinfoComponent,
   ],
   imports: [
     BrowserModule,
