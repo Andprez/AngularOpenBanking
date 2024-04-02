85976bfe5b1de113b2736137cd3ad6a69c90cbeb
fernando mendoza <fmendozaguti@hotmail.com>
2024-04-02 17:11:08 -0500

    detalles de forma, cambio de algunos nombres de etiquetas, ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { terminosycondisionesinicioComponent } from './components/terminosycondisionesinicio/terminosycondisionesinicio.component';
 import { AceptacionTerminosYCondComponent } from './components/aceptacionterminosycond/aceptacionterminosycond.component';
-import { LogoCbitComponent } from './components/header/logo-cbit/logo-cbit.component';
 
 @NgModule({
   declarations: [
--- src/app/components/aceptacionterminosycond/aceptacionterminosycond.component.ts
+++ src/app/components/aceptacionterminosycond/aceptacionterminosycond.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-//import { LogoCbitComponent } from '../header/logo-cbit/logo-cbit.component';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-aceptacionterminosycond',
   templateUrl: './aceptacionterminosycond.component.html',
-  styleUrls: ['./aceptacionterminosycond.component.css'],
-
+  styleUrls: ['./aceptacionterminosycond.component.css']
 })
 export class AceptacionTerminosYCondComponent{
   constructor(private router: Router){
