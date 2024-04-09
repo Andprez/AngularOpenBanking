22a204b8e6e9b1f946815d02e52e9b3adb6517c1
PereAnd <aa.pereza1@uniandes.edu.co>
2024-04-09 14:13:57 -0500

    Animación del login OK

--- src/app/pages/loading-begin/loading-begin.component.ts
+++ src/app/pages/loading-begin/loading-begin.component.ts
@@ -6,48 +6,8 @@ import { ClientesService } from 'src/app/services/clientes.service';
 @Component({
   selector: 'app-loading-begin',
   templateUrl: './loading-begin.component.html',
-  styleUrls: ['./loading-begin.component.css']
+  styleUrls: ['./loading-begin.component.css', '../../templates/background2.css']
 })
 export class LoadingBeginComponent {
-  formLogin!: FormGroup;
-  tiposIdentificacion!: TipoIdentificacion[];
-  clienteExiste: boolean = true;
-  loading: boolean = true;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private clienteService: ClientesService
-  ) {}
-
-  ngOnInit(): void {
-    this.clienteService.getTiposIdentificacion().subscribe((data) => {
-      this.tiposIdentificacion = data;
-    });
-    this.formLogin = this.formBuilder.group({
-      docType: ['', Validators.required],
-      docNumber: ['', Validators.required]
-    });
-  }
-
-  //accion del btn
-  login(): void {
-    let cliente = {
-      tipoIdentificacion: this.formLogin.get('docType')?.value,
-      numeroIdentificacion: this.formLogin.get('docNumber')?.value,
-    };
-    this.clienteService.getBilletera(cliente).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.clienteExiste = true;
-        // if (response) {
-        //   this.generalService.setClienteBilletera(response);
-        // }
-      },
-      error: (error) => {
-        console.log(error);
-        this.clienteExiste = false;
-      },
-    });
-    console.log(this.formLogin.value);
-  }
 }
