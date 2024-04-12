89f30622571ef847c9740010eed308a8deeee838
PereAnd <aa.pereza1@uniandes.edu.co>
2024-04-12 12:17:12 -0500

    Agregado add-product

--- src/app/components/headers/header-entity/header-entity.component.ts
+++ src/app/components/headers/header-entity/header-entity.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header-entity',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-entity.component.css']
 })
 export class HeaderEntityComponent {
-
+  @Input() nameEntity!: string;
+  @Input() nameImage!: string;
 }
--- src/app/pages/add-product/add-product.component.ts
+++ src/app/pages/add-product/add-product.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  styleUrls: ['./add-product.component.css', '../../templates/background3.css']
 })
 export class AddProductComponent {
+  products: string[] = ['Cuenta Ahorros Móvil', 'Crédito Móvil', 'Tarjeta de Crédito'];
+  selectedProduct: string = '';
 
+  formProducto!: FormGroup;
+  formValidation!: FormGroup;
+  showAdditionalFields = false;
+  showSuccessMessage = false;
+
+  constructor(private fb: FormBuilder) {
+    this.formProducto = this.fb.group({
+      product: ['', Validators.required]
+    });
+    this.formValidation = this.fb.group({
+      numCuenta: ['', [Validators.pattern('^[0-9]+$'), Validators.required]],
+      clave: ['', [Validators.minLength(8), Validators.required]],
+    });
+  }
+
+  onSubmitProduct(): void {
+    console.log('Formulario producto:', this.formProducto.value);
+    this.selectedProduct = this.formProducto.get('product')?.value;
+    this.showAdditionalFields = true;
+
+  }
+  onSubmitValidation(): void {
+    console.log('Formulario validación:', this.formValidation.value);
+    this.showSuccessMessage = true;
+  }
 }
