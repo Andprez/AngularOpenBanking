d91eba99d3d46ec1b34d98ef64bfbb4c22db8211
KevinDPantoja <kevinpantoja@cbit-online.com>
2024-04-15 11:56:38 -0500

    Se agrega componente form-document

--- src/app/pages/form-document/form-document.component.ts
+++ src/app/pages/form-document/form-document.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-form-document',
   templateUrl: './form-document.component.html',
-  styleUrls: ['./form-document.component.css']
+  styleUrls: ['./form-document.component.css', '../../templates/background2.css']
 })
 export class FormDocumentComponent {
+
+  texto: string = 'Continuar'
+  postCard: boolean = false
+
+  saveData() {
+    if (!this.postCard) {
+      this.postCard = true
+    }
+    else {
+      console.log ("Guardando datos")
+    }
+  }
 
 }
