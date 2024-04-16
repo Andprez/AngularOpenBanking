ae43f532eb340003208460cda0e7960e48153dbf
PereAnd <aa.pereza1@uniandes.edu.co>
2024-04-15 23:25:26 -0500

    Modificación del app-camera

--- src/app/pages/form-selfie/form-selfie.component.ts
+++ src/app/pages/form-selfie/form-selfie.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-selfie.component.css', '../../templates/background2.css']
 })
 export class FormSelfieComponent {
+  imageData: string = '';
 
+  savePhoto(imageData: string): void {
+    this.imageData = imageData;
+    console.log('Imagen guardada:', this.imageData);
+  }
 }
