5d2e8fe2588fea4ff63c83a1f1e8ba952b62baaa
PereAnd <aa.pereza1@uniandes.edu.co>
2024-04-19 11:28:21 -0500

    Solución de errores en gráfico de finance

--- src/app/components/lists/list-products/list-products.component.ts
+++ src/app/components/lists/list-products/list-products.component.ts
@@ -55,32 +55,4 @@ export class ListProductsComponent implements OnInit {
   setProductSelected(product: any) {
     this.onProductSelected.emit(product);
   }
-
-  products: any[] = [
-    {
-      img: '../../../../assets/entidadesF/daviplata.png',
-      tipoProd: 'Tarjeta de Crédito',
-      montoProd: 3209434,
-    },
-    {
-      img: '../../../../assets/entidadesF/ban-bogota.png',
-      tipoProd: 'Cuenta de Ahorros',
-      montoProd: 3209434,
-    },
-    {
-      img: '../../../../assets/entidadesF/bancolombia.png',
-      tipoProd: 'Cuenta Corriente',
-      montoProd: 3209434,
-    },
-    {
-      img: '../../../../assets/entidadesF/nubank.png',
-      tipoProd: 'Crédito de Vivienda',
-      montoProd: 3209434,
-    },
-    {
-      img: '../../../../assets/entidadesF/pibank.png',
-      tipoProd: 'Crédito de Vehículo',
-      montoProd: 3209434,
-    },
-  ];
 }
--- src/app/components/utils/finance/finance.component.ts
+++ src/app/components/utils/finance/finance.component.ts
@@ -2,81 +2,82 @@ import { Component } from '@angular/core';
 import { Transaction } from 'src/app/models/transaction';
 import { TransaccionService } from 'src/app/services/transaccion.service';
 import Chart from 'chart.js/auto';
+import { TipoTransaccion } from 'src/app/models/tipo-transaccion';
+import { forkJoin } from 'rxjs';
+import { ProductoF } from 'src/app/models/producto-f';
+import { ProductosFService } from 'src/app/services/productos-f.service';
+import { Cliente } from 'src/app/models/cliente';
 
 @Component({
   selector: 'app-finance',
   templateUrl: './finance.component.html',
-  styleUrls: ['./finance.component.css']
+  styleUrls: ['./finance.component.css'],
 })
 export class FinanceComponent {
-  //acceder al servicio finanzas
+  productsByClient: ProductoF[] = [];
+  transactions: Transaction[] = [];
+  typesTransactions: TipoTransaccion[] = [];
+  transactionsByType: any = {};
+  totalByType: any = {};
+  user: Cliente = {} as Cliente;
+
   constructor(
-    private transfers: TransaccionService) { }
+    private transactionsService: TransaccionService,
+    private productsFService: ProductosFService
+  ) {}
 
   public chart: any;
   public montoTransacciones: number = 0;
-  // 'Otros', 'Pagos', 'Transferencias', 'Compras'
   public montoCompras: number = 0;
   public montoCreditos: number = 0;
   public montoServiciosPub: number = 0;
   public montoPagos: number = 0;
 
   ngOnInit(): void {
-    this.obtenerTransferencia();
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+    this.getTransactionsByClientId(this.user.idCliente!);
   }
 
-  obtenerTransferencia() {
-    this.transfers.getTransaccion().subscribe({
-      next: transacciones => {
-        transacciones.forEach((transaccion: Transaction) => {
-          this.montoTransacciones += transaccion.montoTransaccion;
-          let tipo = transaccion.idTipo_Transaccion
-          if (tipo == 1) { this.montoCompras += transaccion.montoTransaccion }
-          if (tipo == 2) { this.montoCreditos += transaccion.montoTransaccion }
-          if (tipo == 3) { this.montoServiciosPub += transaccion.montoTransaccion }
-          if (tipo == 4) { this.montoPagos += transaccion.montoTransaccion }
-        });
-        console.log("Valores", this.montoCompras, this.montoCreditos, this.montoServiciosPub, this.montoPagos);
+  getTransactionsByClientId(userId: number){
+    const reqTransactions = this.transactionsService.getTransactionsByClientId(userId);
+    const reqTypesTransactions = this.transactionsService.getTypesTransactions();
+
+    forkJoin([reqTransactions, reqTypesTransactions]).subscribe({
+      next: ([transactions, types]) => {
+        this.transactions = transactions;
+        this.typesTransactions = types;
+
+        transactions.forEach( transaction => {
+          let type = this.typesTransactions.find(tt => tt.idTipo_Transaccion === transaction.idTipo_Transaccion);
+          this.transactionsByType[type?.nombreTipo!] = (this.transactionsByType[type?.nombreTipo!] || 0) + transaction.montoTransaccion;
+        })
+        console.log(this.transactionsByType);
         this.crearPie();
       },
-      error: (err) => {
-        console.log("Ocurrió un error")
-      }
     });
-}
-crearPie() {
-  this.chart = new Chart('myChart', {
-    type: 'doughnut',
-    data: {
+  }
 
-      //Compras, Retiros, Transferencias, otros(4*1000/cuota de manejo),Pagos()
-      //movimientos Egreso (Tarjetacredito, cuentas (ahorros, moviles(monederos), corrientes), )
-      labels: ['Otros', 'Créditos', 'Servicios públicos', 'Pagos'],
-      datasets: [{
-        label: 'Tus Gastos',
-        data: [
-          this.montoCompras,
-          this.montoCreditos,
-          this.montoServiciosPub,
-          this.montoPagos
+  crearPie() {
+    this.chart = new Chart('myChart', {
+      type: 'doughnut',
+      data: {
+        labels: Object.keys(this.transactionsByType),
+        datasets: [
+          {
+            label: 'Tus Gastos',
+            data: Object.values(this.transactionsByType),
+            backgroundColor: [
+              'rgb(115,220,248)',
+              'rgb(70, 188, 219)',
+              'rgb(42, 117, 165)',
+              'rgb(6, 67, 106)',
+            ],
+            hoverOffset: 4,
+            borderWidth: 1,
+          },
         ],
-        backgroundColor: [
-          'rgb(115,220,248)',
-          'rgb(70, 188, 219)',
-          'rgb(42, 117, 165)',
-          'rgb(6, 67, 106)',
-        ],
-        hoverOffset: 4,
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-}
+      },
+      options: {},
+    });
+  }
 }
