a3995469b5780477da5cd7a1c4d26233a945f747
fernando mendoza <fmendozaguti@hotmail.com>
2024-07-31 13:35:38 -0500

    se trabaja sobre el typescript de credit disburse

--- src/app/pages/credit-disburse/credit-disburse.component.ts
+++ src/app/pages/credit-disburse/credit-disburse.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-credit-disburse',
   templateUrl: './credit-disburse.component.html',
   styleUrls: ['./credit-disburse.component.css']
 })
-export class CreditDisburseComponent {
+export class CreditDisburseComponent implements OnInit{
+  creditData: any={};
+
+
+
+  ngOnInit(): void {
+    // Recuperar datos del local storage
+    this.creditData = JSON.parse(localStorage.getItem("creditData")!);
+
+
+    // Obtener variables con datos de los componentes anteriores por medio de local storage
+
+  }
 
 }
